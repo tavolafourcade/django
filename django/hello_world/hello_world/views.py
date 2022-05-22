81e7a6a62bd265ce263e185d848ec3bb87db7b9a
tavolafourcade <tavolafourcade@gmail.com>
2022-05-21 21:39:01 -0500

    [ ADD ] Pass arguments within the context

--- django/hello_world/hello_world/views.py
+++ django/hello_world/hello_world/views.py
@@ -2,12 +2,18 @@ from django.http import HttpResponse
 from django.template import Template, Context
 
 def first_template(self):
+
+  name = 'John'
+  last_name = 'Doe'
+
+  dictionary = {'name': name, 'last_name': last_name}
+
   html_path = open('/Users/octaviolafourcade/Documents/PYTHON/django/hello_world/hello_world/templates/first_template.html')
 
   template = Template(html_path.read())
   html_path.close()
 
-  context = Context()
+  context = Context(dictionary)
 
   document = template.render(context)
 
