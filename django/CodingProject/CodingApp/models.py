140e172426f9046ae48fbd9b78ceec561c5d305e
tavolafourcade <tavolafourcade@gmail.com>
2022-05-22 11:26:49 -0500

    [ ADD ] Creating the Model for Student, Professor, Task and Course

--- django/CodingProject/CodingApp/models.py
+++ django/CodingProject/CodingApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+  name = models.CharField(max_length=40)
+  last_name = models.CharField(max_length=40)
+  email = models.EmailField()
+
+class Professor(models.Model):
+  name = models.CharField(max_length=40)
+  last_name = models.CharField(max_length=40)
+  email = models.EmailField()
+  field = models.CharField(max_length=40)
+
+class Task(models.Model):
+  name= models.CharField(max_length=30)
+  delivery_date = models.DateField()  
+  delivered = models.BooleanField()
+
+class Course(models.Model):
+  name = models.CharField(max_length=40)
+  course_id = models.IntegerField()
