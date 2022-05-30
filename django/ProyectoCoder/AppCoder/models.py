ab555f9366407d896f6ebb3568ddc02dfc2236f3
tavolafourcade <tavolafourcade@gmail.com>
2022-05-29 21:05:41 -0500

    [ ADD ] Implementation of the admin panel to work with the model

--- django/ProyectoCoder/AppCoder/models.py
+++ django/ProyectoCoder/AppCoder/models.py
@@ -4,6 +4,9 @@ class Curso(models.Model):
   nombre = models.CharField(max_length=40)
   camada = models.IntegerField()
 
+  def __str__(self) -> str:
+    return self.nombre+" "+str(self.camada)
+
 class Estudiante(models.Model):
   nombre = models.CharField(max_length=30)
   apellido = models.CharField(max_length=30)
