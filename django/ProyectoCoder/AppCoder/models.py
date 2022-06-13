1d3b750b62de319bca7d506bee1ca653343ade3d
tavolafourcade <tavolafourcade@gmail.com>
2022-06-13 16:51:59 -0500

    [ ADD ] Upload avatar

--- django/ProyectoCoder/AppCoder/models.py
+++ django/ProyectoCoder/AppCoder/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Curso(models.Model):
   nombre = models.CharField(max_length=40)
   camada = models.IntegerField()
@@ -28,3 +28,7 @@ class Entregable(models.Model):
   nombre = models.CharField(max_length=30)
   fechaDeEntrega = models.DateField()
   entregado = models.BooleanField()
+
+class Avatar(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
